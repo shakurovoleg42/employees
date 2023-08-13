24031855e37e2a9941bf012e3b2ec416319a871c
Oleg Shakurov <thorsonodin2003@gmail.com>
2023-08-13 14:14:30 +0500

    test

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -23,7 +23,7 @@ function Navbars() {
     <Container>
      {/* <Navbar.Brand href="/">
      </Navbar.Brand> */}
-     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+     {/* <Navbar.Toggle aria-controls="responsive-navbar-nav" /> */}
      <Navbar.Collapse id="responsive-navbar-nav">
       <Nav className="me-auto">
        <Nav.Item className="text-white">User: {user.name}</Nav.Item>
